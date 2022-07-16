c922ab58c54fe1aa17e051028c36dcfb8e90175d
Ayoub Basidi <93930160+AYOUBBASIDI@users.noreply.github.com>
2022-07-16 02:24:41 +0100

    add file project

---
+++ index.js
@@ -0,0 +1,19 @@
+const jsonfile = require('jsonfile');
+const moment = require('moment');
+const simpleGit = require('simple-git');
+const random = require('random');
+const FILE_PATH = './data.json';
+
+const makeCommit = n => {
+    if(n===0) return simpleGit().push();
+    const DATE = moment().subtract(1,'y').add(7,'w').add(8,'d').format();
+    const data = {
+        date: DATE
+    }
+    console.log(DATE);
+    jsonfile.writeFile(FILE_PATH, data, ()=>{
+        simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE },makeCommit.bind(this,--n));
+    });
+}
+
+makeCommit(10)
